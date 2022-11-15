17957acb0d86713f9af3bc45bcb711927f66466b
Dimitris <dim.makris98@gmail.com>
2022-11-15 16:59:42 +0200

    blabla

---
+++ script.js
@@ -0,0 +1,97 @@
+let currentTotal = 0;
+let buffer = "0";
+
+let previousOperator = null;
+
+const calcScreen = document.querySelector(".calc-numbers");
+
+document.querySelector('.calculator-buttons').addEventListener("click", function(event){
+    buttonClick(event.target.innerHTML);
+})
+
+function buttonClick(value){
+    if(isNaN(parseInt(value))){
+        handleSymbol(value);
+    }
+    else{
+        handleNumber(value);
+    }
+    rendererScreen();
+}
+
+function handleSymbol(value){
+    switch(value){
+        case "C":
+            buffer = "0";
+            currentTotal = 0;
+            previousOperator = null;
+            break;
+        case "=":
+            if(previousOperator === null){
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            buffer = "" + currentTotal;
+            previousOperator = null;
+            currentTotal = 0;
+            break;
+        case "←":
+            if(buffer.length === 1){ //screen depicts only 1 number
+                buffer = "0";
+            }
+            else{
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        default:
+            handleMath(value);
+            break;
+    }
+}
+
+function handleNumber(value){
+    if(buffer === "0"){
+        buffer = value;
+    }
+    else{
+        buffer += value;
+    }
+}
+
+function handleMath(value){
+    const internalBuffer = parseInt(buffer);
+
+    if(currentTotal === 0){
+        currentTotal = internalBuffer;
+    }
+    else{
+        flushOperation(internalBuffer);
+    }
+
+    previousOperator = value;
+    buffer = "0";
+}
+
+function flushOperation(internalBuffer){
+    if(previousOperator === "+"){
+        currentTotal += internalBuffer;
+    }
+    else if(previousOperator === "-"){
+        currentTotal -= internalBuffer;
+    }
+    else if(previousOperator === "x"){
+        currentTotal *= internalBuffer;
+    }
+    else{
+        if(internalBuffer !== 0){
+            currentTotal /= internalBuffer;
+        }
+        else{
+            currentTotal = "Error!";
+        }
+    }
+}
+
+function rendererScreen(){
+    calcScreen.value = buffer;
+}
